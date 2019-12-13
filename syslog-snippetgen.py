79e1c2d9d0c7203452ce6be81e9a77d1a9c09642
Attila Szakacs <attila.szakacs@balabit.com>
2019-12-13 17:35:38 +0100

    syslog-snippetgen: first steps of creating snippet from options
    Signed-off-by: Attila Szakacs <attila.szakacs@balabit.com>

--- syslog-snippetgen.py
+++ syslog-snippetgen.py
@@ -7,12 +7,24 @@ def create_snippet_entry(snippetname, prefix, body):
 
 filename = "It_s_a_snippet"
 fullname = filename + ".code-snippets"
-snippetname = "snippetname"
-prefix = "prefix"
-body = "body"
+options = [
+    ("destination", "kafka-c", "bootstrap-servers", ["<string>"]),
+    ("destination", "kafka-c", "client-lib-dir", ["<string>"]),
+    ("destination", "kafka-c", "flush-timeout-on-reload", ["<number>"]),
+    ("destination", "kafka-c", "flush-timeout-on-shutdown", ["<number>"])
+]
 
-snippet = create_snippet_entry(snippetname, prefix, body)
+def create_snippet_entries():
+    entries = []
+    for context, driver, keyword, arguments in options:
+        # Refine these, build up the body in the correct way, add the hint for all the other options etc...
+        # See https://github.com/DONSOC/Vscode_snippets/blob/master/kafka.code-snippets#L49
+        snippetname = keyword.replace("-", "")
+        prefix = context + "-" + driver + "::" + keyword
+        body = keyword + "(" + " ".join(arguments) + ")"
+        entries.append(create_snippet_entry(snippetname, prefix, body))
+    return entries
 
 with open(fullname, "w") as f: # write snippet to file
-    json.dump(snippet, f, indent=4)
+    json.dump(create_snippet_entries(), f, indent=4)
     f.close()
