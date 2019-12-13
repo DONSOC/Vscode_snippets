afc018174799ddd7ae1d2272cb2c985e0fba1c03
Attila Szakacs <attila.szakacs@balabit.com>
2019-12-13 17:23:42 +0100

    syslog-snippetgen: move snippet entry creation to separate function
    Signed-off-by: Attila Szakacs <attila.szakacs@balabit.com>

--- syslog-snippetgen.py
+++ syslog-snippetgen.py
@@ -2,15 +2,16 @@ import json
 
 # input as a list
 
-filename = "It's_a_snippet"
+def create_snippet_entry(snippetname, prefix, body):
+    return { snippetname: { "prefix": prefix, "body": [ body ] } }
+
+filename = "It_s_a_snippet"
 fullname = filename + ".code-snippets"
 snippetname = "snippetname"
 prefix = "prefix"
 body = "body"
 
-
-snippet = { snippetname: { "prefix": prefix, "body": [ body ] } }
-
+snippet = create_snippet_entry(snippetname, prefix, body)
 
 with open(fullname, "w") as f: # write snippet to file
     json.dump(snippet, f, indent=4)
